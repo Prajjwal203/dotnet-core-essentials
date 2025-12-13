2d2ff3695da32701892bb286a07e0064c85d153e
Prajjwal <prajjwals203@gmail.com>
2025-12-13 17:43:09 +0530

    chore: made a test controller to test API through Swagger

---
+++ src/DotNetCoreEssentials.Api/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreEssentials.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "API is working" });
+        }
+    }
+}
